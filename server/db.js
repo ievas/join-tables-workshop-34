4dca628604ce32f70d7cf1044f4794f5ee51750a
ievas <ieva.ritina@gmail.com>
2024-04-18 20:53:35 -0600

    routes added

--- server/db.js
+++ server/db.js
@@ -2,14 +2,14 @@ let pg = require("pg");
 let client = new pg.Client(
   process.env.DATABASE_URL || "postgres://localhost/acme_reservations_db"
 );
+let uuid = require("uuid");
 
 let createTables = async () => {
-  //customer(id, name); restaurant(id, name);
-  //reservation(id, date, party_count, restaurant_id, customer_id)
   let SQL = `
+  DROP TABLE IF EXISTS reservations;
   DROP TABLE IF EXISTS customers;
   DROP TABLE IF EXISTS restaurants;
-  DROP TABLE IF EXISTS reservations;
+  
 
   CREATE TABLE customers(
     id UUID PRIMARY KEY,
@@ -30,7 +30,60 @@ let createTables = async () => {
   await client.query(SQL);
 };
 
+let createCustomer = async (name) => {
+  let SQL = `INSERT INTO customers(id, name) VALUES ($1,$2) RETURNING *`;
+  let response = await client.query(SQL, [uuid.v4(), name]);
+  return response.rows[0];
+};
+let createRestaurant = async (name) => {
+  let SQL = `INSERT INTO restaurants(id, name) VALUES ($1,$2) RETURNING *`;
+  let response = await client.query(SQL, [uuid.v4(), name]);
+  return response.rows[0];
+};
+let fetchCustomers = async () => {
+  let SQL = `SELECT * FROM customers`;
+  let response = await client.query(SQL);
+  return response.rows;
+};
+let fetchRestaurants = async () => {
+  let SQL = `SELECT * FROM restaurants`;
+  let response = await client.query(SQL);
+  return response.rows;
+};
+let fetchReservations = async () => {
+  let SQL = `SELECT * FROM reservations`;
+  let response = await client.query(SQL);
+  return response.rows;
+};
+let createReservation = async ({
+  date,
+  party_count,
+  restaurant_id,
+  customer_id,
+}) => {
+  let SQL = `INSERT INTO reservations (id, date, party_count, restaurant_id, customer_id) VALUES ($1,$2,$3,$4,$5) RETURNING *`;
+  let response = await client.query(SQL, [
+    uuid.v4(),
+    date,
+    party_count,
+    restaurant_id,
+    customer_id,
+  ]);
+  return response.rows[0];
+};
+let destroyReservation = async (id) => {
+  let SQL = `DELETE FROM reservations WHERE id=$1`;
+  await client.query(SQL, [id]);
+};
+
 module.exports = {
   client,
   createTables,
+  createCustomer,
+  createRestaurant,
+  fetchCustomers,
+  fetchRestaurants,
+  fetchReservations,
+  createReservation,
+  destroyReservation,
 };
--- server/index.js
+++ server/index.js
@@ -1,10 +1,79 @@
-let { client, createTables } = require("./db");
+let {
+  client,
+  createTables,
+  createCustomer,
+  createRestaurant,
+  fetchCustomers,
+  fetchRestaurants,
+  createReservation,
+  destroyReservation,
+  fetchReservations,
+} = require("./db");
+
+let express = require("express");
+let app = express();
+app.use(express.json());
+
+app.get("/api/customers", async (req, res, next) => {
+  try {
+    res.send(await fetchCustomers());
+  } catch (error) {
+    next(error);
+  }
+});
+app.get("/api/restaurants", async (req, res, next) => {
+  try {
+    res.send(await fetchRestaurants());
+  } catch (error) {
+    next(error);
+  }
+});
+app.get("/api/reservations", async (req, res, next) => {
+  try {
+    res.send(await fetchReservations());
+  } catch (error) {
+    next(error);
+  }
+});
+app.post("/api/customers", async (req, res, next) => {
+  try {
+    res.status(201).send(await createCustomer(req.body.name));
+  } catch (error) {
+    next(error);
+  }
+});
+app.post("/api/restaurants", async (req, res, next) => {
+  try {
+    res.status(201).send(await createRestaurant(req.body.name));
+  } catch (error) {
+    next(error);
+  }
+});
+app.post("/api/reservations", async (req, res, next) => {
+  try {
+    res.status(201).send(await createReservation(req.body));
+  } catch (error) {
+    next(error);
+  }
+});
+app.delete("/api/reservations/:id", async (req, res, next) => {
+  try {
+    await destroyReservation(req.params.id);
+    // res.sendStatus(204);
+    res.json({ message: "reservation deleted" });
+  } catch (ex) {
+    next(ex);
+  }
+});
 
 async function init() {
   await client.connect();
   console.log("connected to database");
   await createTables();
   console.log("tables created");
+
+  let port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`listening on port ${port}`));
 }
 
 init();
